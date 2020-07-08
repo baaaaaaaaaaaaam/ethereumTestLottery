b50b61959a7f3aef39860ceb7e890722628aaf6e
baaaaaaaaaaaaam <ansgyqja@naver.com>
2020-07-09 02:10:37 +0900

    수정

--- lottery-web/src/App.js
+++ lottery-web/src/App.js
@@ -7,12 +7,14 @@ import Web3 from 'web3';
 
 
 // 컨트랙트를 재배포 할경우 컨트랙트 address가  재생성되 변경 해주어야 한다
-let lotteryAddress = '0x78d8e46FCC55CD6aAC05013a3C322449F65bA8D6';
+let lotteryAddress = '0x1544fdEbb4B39BE507A925Bacf89A69c590947E4';
 let lotteryABI = [{ "constant": true, "inputs": [], "name": "answerForTest", "outputs": [{ "name": "", "type": "bytes32" }], "payable": false, "stateMutability": "view", "type": "function", "signature": "0x84f7e4f0" }, { "constant": true, "inputs": [], "name": "owner", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function", "signature": "0x8da5cb5b" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor", "signature": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "index", "type": "uint256" }, { "indexed": false, "name": "bettor", "type": "address" }, { "indexed": false, "name": "amount", "type": "uint256" }, { "indexed": false, "name": "challenges", "type": "bytes1" }, { "indexed": false, "name": "answerBlockNumber", "type": "uint256" }], "name": "BET", "type": "event", "signature": "0x100791de9f40bf2d56ffa6dc5597d2fd0b2703ea70bc7548cd74c04f5d215ab7" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "index", "type": "uint256" }, { "indexed": false, "name": "bettor", "type": "address" }, { "indexed": false, "name": "amount", "type": "uint256" }, { "indexed": false, "name": "challenges", "type": "bytes1" }, { "indexed": false, "name": "answer", "type": "bytes1" }, { "indexed": false, "name": "answerBlockNumber", "type": "uint256" }], "name": "WIN", "type": "event", "signature": "0x8219079e2d6c1192fb0ff7f78e6faaf5528ad6687e69749205d87bd4b156912b" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "index", "type": "uint256" }, { "indexed": false, "name": "bettor", "type": "address" }, { "indexed": false, "name": "amount", "type": "uint256" }, { "indexed": false, "name": "challenges", "type": "bytes1" }, { "indexed": false, "name": "answer", "type": "bytes1" }, { "indexed": false, "name": "answerBlockNumber", "type": "uint256" }], "name": "FAIL", "type": "event", "signature": "0x3b19d607433249d2ebc766ae82ca3848e9c064f1febb5147bc6e5b21d0adebc5" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "index", "type": "uint256" }, { "indexed": false, "name": "bettor", "type": "address" }, { "indexed": false, "name": "amount", "type": "uint256" }, { "indexed": false, "name": "challenges", "type": "bytes1" }, { "indexed": false, "name": "answer", "type": "bytes1" }, { "indexed": false, "name": "answerBlockNumber", "type": "uint256" }], "name": "DRAW", "type": "event", "signature": "0x72ec2e949e4fad9380f9d5db3e2ed0e71cf22c51d8d66424508bdc761a3f4b0e" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "index", "type": "uint256" }, { "indexed": false, "name": "bettor", "type": "address" }, { "indexed": false, "name": "amount", "type": "uint256" }, { "indexed": false, "name": "challenges", "type": "bytes1" }, { "indexed": false, "name": "answerBlockNumber", "type": "uint256" }], "name": "REFUND", "type": "event", "signature": "0x59c0185881271a0f53d43e6ab9310091408f9e0ff9ae2512613de800f26b8de4" }, { "constant": true, "inputs": [], "name": "getPot", "outputs": [{ "name": "pot", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function", "signature": "0x403c9fa8" }, { "constant": false, "inputs": [{ "name": "challenges", "type": "bytes1" }], "name": "betAndDistribute", "outputs": [{ "name": "result", "type": "bool" }], "payable": true, "stateMutability": "payable", "type": "function", "signature": "0xe16ea857" }, { "constant": false, "inputs": [{ "name": "challenges", "type": "bytes1" }], "name": "bet", "outputs": [{ "name": "result", "type": "bool" }], "payable": true, "stateMutability": "payable", "type": "function", "signature": "0xf4b46f5b" }, { "constant": false, "inputs": [], "name": "distribute", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function", "signature": "0xe4fc6b6d" }, { "constant": false, "inputs": [{ "name": "answer", "type": "bytes32" }], "name": "setAnswerForTest", "outputs": [{ "name": "result", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function", "signature": "0x7009fa36" }, { "constant": true, "inputs": [{ "name": "challenges", "type": "bytes1" }, { "name": "answer", "type": "bytes32" }], "name": "isMatch", "outputs": [{ "name": "", "type": "uint8" }], "payable": false, "stateMutability": "pure", "type": "function", "signature": "0x99a167d7" }, { "constant": true, "inputs": [{ "name": "index", "type": "uint256" }], "name": "getBetInfo", "outputs": [{ "name": "answerBlockNumber", "type": "uint256" }, { "name": "bettor", "type": "address" }, { "name": "challenges", "type": "bytes1" }], "payable": false, "stateMutability": "view", "type": "function", "signature": "0x79141f80" }];
 
 
 class App extends Component {
 
+
+  // 객체 생성자 
   constructor(props) {
     super(props);
 
@@ -33,6 +35,8 @@ class App extends Component {
     }
   }
 
+
+  //웹실행시 비동기 방식으로 가장먼저 실행되는부분
   async componentDidMount() {
     await this.initWeb3();
     await this.polData();
@@ -40,53 +44,136 @@ class App extends Component {
     // setInterval(this.polData,1000);
   }
 
-  polData = async() =>{
-    await this.getPot();
-    await this.getBetEvent();
-    await this.getWinEvent();
-    await this.getFailEvent();
-    this.makeFinalRecords();
-  }
-  initWeb3 = async () => {
-    if (window.ethereum) {
-      this.web3 = new Web3(window.ethereum);
-      try {
-        // Request account access if needed
-        window.ethereum.enable();
-      } catch (error) {
-        // User denied account access…
-      }
-    } else if (window.web3) {
-      // Legacy dapp browsers…
-      window.web3 = new Web3(window.web3.currentProvider);
-    } else {
-      // Non-dapp browsers…
-      console.log(
-        'Non-Ethereum browser detected. You should consider trying Status!'
-      );
+
+ // web3를 시작하여 metamast와 연결하는 부분 
+ initWeb3 = async () => {
+  if (window.ethereum) {
+    this.web3 = new Web3(window.ethereum);
+    try {
+      // Request account access if needed
+      window.ethereum.enable();
+    } catch (error) {
+      // User denied account access…
     }
-    let accounts = await this.web3.eth.getAccounts();
-    this.accounts = accounts[0];
-    this.lotteryContract = new this.web3.eth.Contract(lotteryABI, lotteryAddress);
+  } else if (window.web3) {
+    // Legacy dapp browsers…
+    window.web3 = new Web3(window.web3.currentProvider);
+  } else {
+    // Non-dapp browsers…
+    console.log(
+      'Non-Ethereum browser detected. You should consider trying Status!'
+    );
+  }
+  let accounts = await this.web3.eth.getAccounts();
+  this.accounts = accounts[0];
+  this.lotteryContract = new this.web3.eth.Contract(lotteryABI, lotteryAddress);
 
 
-    // call 스마트컨트랙트의 값을 불러오기만함 / invoke , send 는 값을 입력할수잇음
-   
-    // let owner =await this.lotteryContract.methods.owner().call();
-    // console.log(owner);
+  // call 스마트컨트랙트의 값을 불러오기만함 / invoke , send 는 값을 입력할수잇음
+ 
+  // let owner =await this.lotteryContract.methods.owner().call();
+  // console.log(owner);
+}
+
+  polData = async() =>{
+    //  실행시 컨트랙트가 저장하고있는 블록의 데이터를 불러오는 부분 !!!
+    //  컨트랙트는 자신이 해당하는 정보의 블록을 Queue스택에 저장해 놓는다 
+    await this.getPot();   // ==> 1
+    await this.getBetEvent();  // ==> 2
+    await this.getWinEvent();   // ==> 3 
+    await this.getFailEvent();  // ==> 4
+    this.makeFinalRecords();  // ==> 5
   }
 
-
-  // getPot() 실행하여 현재 bet에 금액 얼만지 확인
+  //  1.
+  //  누적 상금인 bet의  금액 얼만지 확인
   // 이후 DidMount 에서 interval로 주기적으로 불러올 예정
   getPot = async()=>{
-    let pot = await this.lotteryContract.methods.getPot().call();
-    let potString = this.web3.utils.fromWei(pot.toString(),'ether');
-    this.setState({pot:potString});
+    let pot = await this.lotteryContract.methods.getPot().call();  //call 을하는 부분에서 트랜잭션 비용이 발생하지않는다 ! 정보를 받아올뿐 
+    let potString = this.web3.utils.fromWei(pot.toString(),'ether');  // 받아온 pot 은 BN을 toString 으로 변환함, 단위는  ether단위로 
+    this.setState({pot:potString});    // 불러온 값을 위에 state의 pot에 적용 한다.
   }
 
 
+  // 2.Contract에 있는 BET 이벤트 
+  // event BET(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBlockNumber);
+  getBetEvent = async () => {
 
+    // records라는 배열을 만든다 ( 어떤게 들어가는지 확인 필요 )
+    const records = [];
+
+    //contract 에 생성한 event중 'BET'이란 이름을 가진 로그를 처음부터 끝까지 불러와 betEvent에 담는다
+    let betEvent = await this.lotteryContract.getPastEvents('BET', { fromBlock: 0, toBlock: 'latest' });
+    
+
+    //가져온이벤트 배열을 처음부터 끝까지 돌린다
+    for(let i=0;i<betEvent.length;i+=1){
+      //  json형태의 record변수를 만든다.
+      const record = {}
+
+      // index정보를 가져오는데 인덱스의 숫자의 자리숫는 10자리까지 가능하다.
+      record.index=parseInt(betEvent[i].returnValues.index,10).toString();
+
+      // 이벤트 bettor에 있는 값
+      record.bettor=betEvent[i].returnValues.bettor;
+
+      // 이벤트 변수에는 없지만 해당 블록 넘버를 불러올수있음
+      record.betBlockNumber = betEvent[i].blockNumber;
+
+      // 이벤트에 적어둔 정답을 맞춰볼 블록 넘버
+      record.targetBlockNumber=betEvent[i].returnValues.answerBlockNumber.toString();
+
+      // 이벤트에 남겨둔 정답 시도 
+      record.challenges=betEvent[i].returnValues.challenges;
+      // 임의의 값
+      record.win='a';
+      // 임의의값
+      record.answer='0x0a';
+
+      // 역순으로 배열에 저장
+      records.unshift(record);
+    }
+
+    this.setState({betRecords:records})
+    console.log(records)
+  }
+
+  //3
+  //  event WIN(uint256 index, address bettor, uint256 amount, byte challenges,byte answer, uint256 answerBlockNumber);
+  getWinEvent = async () => {
+    const records = [];
+    let betEvent = await this.lotteryContract.getPastEvents('WIN', { fromBlock: 0, toBlock: 'latest' });
+    
+    for(let i=0;i<betEvent.length;i+=1){
+      const record = {}
+      record.index=parseInt(betEvent[i].returnValues.index,10).toString();
+      record.amount=parseInt(betEvent[i].returnValues.amount,10).toString();
+      records.unshift(record);
+    }
+    // console.log(records);
+    this.setState({winRecords:records})
+  }
+
+
+  // 4
+  // event FAIL(uint256 index, address bettor, uint256 amount, byte challenges,byte answer, uint256 answerBlockNumber);
+  getFailEvent = async () => {
+    const records = [];
+    let betEvent = await this.lotteryContract.getPastEvents('FAIL', { fromBlock: 0, toBlock: 'latest' });
+    
+    for(let i=0;i<betEvent.length;i+=1){
+      const record = {}
+      record.index=parseInt(betEvent[i].returnValues.index,10).toString();
+      record.answer=betEvent[i].returnValues.answer;
+      records.unshift(record);
+    }
+    // console.log(records);
+    this.setState({failRecords:records})
+  }
+
+
+  // 5
+  // BET , WIN , FAIL 을 하나의 배열로 만듬 
   makeFinalRecords = () =>{
     let f=0 ,w =0 ;
     const records=[...this.state.betRecords];
@@ -105,6 +192,7 @@ class App extends Component {
         if(this.state.failRecords.length-1>f)f++;
 
       }else{
+        // WIN도 아니고 FAIL도 아닌 DRAW 이거나 아직 처리가안됨경우 
         records[i].answer='Not Revealed';
       }
     }
@@ -112,84 +200,25 @@ class App extends Component {
     this.setState({fianlRecords:records})
   }
 
-  getBetEvent = async () => {
-    const records = [];
-    let betEvent = await this.lotteryContract.getPastEvents('BET', { fromBlock: 0, toBlock: 'latest' });
-    
-    for(let i=0;i<betEvent.length;i+=1){
-      const record = {}
-      record.index=parseInt(betEvent[i].returnValues.index,10).toString();
-      record.bettor=betEvent[i].returnValues.bettor;
-      record.betBlockNumber = betEvent[i].blockNumber;
-      record.targetBlockNumber=betEvent[i].returnValues.answerBlockNumber.toString();
-      record.challenges=betEvent[i].returnValues.challenges;
-      record.win='Not Revealed';
-      record.answer='0x0a';
-      records.unshift(record);
-    }
-
-    this.setState({betRecords:records})
-  }
-  getWinEvent = async () => {
-    const records = [];
-    let betEvent = await this.lotteryContract.getPastEvents('WIN', { fromBlock: 0, toBlock: 'latest' });
-    
-    for(let i=0;i<betEvent.length;i+=1){
-      const record = {}
-      record.index=parseInt(betEvent[i].returnValues.index,10).toString();
-      record.amount=parseInt(betEvent[i].returnValues.amount,10).toString();
-      records.unshift(record);
-    }
-    console.log(records);
-    this.setState({winRecords:records})
-  }
-
-  getFailEvent = async () => {
-    const records = [];
-    let betEvent = await this.lotteryContract.getPastEvents('FAIL', { fromBlock: 0, toBlock: 'latest' });
-    
-    for(let i=0;i<betEvent.length;i+=1){
-      const record = {}
-      record.index=parseInt(betEvent[i].returnValues.index,10).toString();
-      record.answer=betEvent[i].returnValues.answer;
-      records.unshift(record);
-    }
-    // console.log(records);
-    this.setState({failRecords:records})
-
-
-  }
+ 
  
 
-  bet = async () => {
-    //nonce 특정 address가 몇개의 트랜잭션을 만들엇는지의 값 , 트랜재션 리플레이 방지 , 외부 유저가 마음대로 사용못하게 방지
-
-    let challenges = '0x' + this.state.challenges[0].toLowerCase() + this.state.challenges[1].toLowerCase();
-    // let nonce = await this.web3.eth.getTransactionCount(this.account);
-    this.lotteryContract.methods.betAndDistribute(challenges).send({ from: this.accounts, value: 5000000000000000, gas: 300000})
-    .on('transactionHash',(hash)=>{
-      console.log(hash)
-    })
-
-  }
 
 
-  // pot money
-
-  // bet 글자 선택 UI
-
-  //  bet button
-
-  // history table   
-  // colum : index , address , challenge , answer ,pot ,status ,answerBlockNumber 
+  //  getCard에서 카드 이미지를 선택할 경우 카드이미지에 배정되어있는 문자를 state 의 challenges의 마지막 문자로 변경하고 기존의 문자를 앞의 문자로 변경시킨다
+  //  ex )  orignal == ab   ,  세번째 이미지 ( 할당된 문자 c ) 클릭 후  bc로 변경됨
   onClickCard = (_Character) =>{
     this.setState({
       challenges:[this.state.challenges[1],_Character]
     })
   }
 
+
+//  카드 이미지 부분 입력할 알파벳과 , 카드의 스타일 정함 
+
   getCard = (_Character, _cardStyle) => {
 
+    // 빈 문자열 변수 만들고 조건에 따라 해당 문제가 들어올경우 이미지로 변경한다
     let _char = '';
     if(_Character==='a'){
       _char='🂡'
@@ -205,6 +234,8 @@ class App extends Component {
     }
 
     return (
+
+      // 해당 카드 스타일을 클릭할경우 생성자에 있는 state.answer의 문자를 js로 변경한다.
       <button className={_cardStyle} onClick={() =>{
           this.onClickCard(_Character)
       }}>
@@ -216,6 +247,15 @@ class App extends Component {
       </button>
     )
   };
+
+
+
+
+
+
+
+ 
+
 
 
   render() {
@@ -280,6 +320,26 @@ class App extends Component {
 
     );
   }
+
+
+//트랜잭션 보내기 
+bet = async () => {
+  //nonce 특정 address가 몇개의 트랜잭션을 만들엇는지의 값 , 트랜재션 리플레이 방지 , 외부 유저가 마음대로 사용못하게 방지
+
+
+  // 트랜잭션에 발송할 challenges 셋팅
+  let challenges = '0x' + this.state.challenges[0].toLowerCase() + this.state.challenges[1].toLowerCase();
+  // let nonce = await this.web3.eth.getTransactionCount(this.account);
+
+  // 실제로 트랜션이 발생하는부분 !!!!!!!!!!!!!!!!!!!!!!
+  this.lotteryContract.methods.betAndDistribute(challenges).send({ from: this.accounts, value: 5000000000000000, gas: 300000})
+  .on('transactionHash',(hash)=>{
+    console.log(hash)
+  })
+
+}
+
+
 }
 
 
--- test_lottery/contracts/Lottery.sol
+++ test_lottery/contracts/Lottery.sol
@@ -13,11 +13,15 @@ contract Lottery {
          byte challenges;
     }
 
-
+    // 돈을 받는 사람
     address payable public owner;
      //  도박에 받을 돈
     uint256 private _pot;
+
+    // 정답을 고정으로 할것인지 아니면 블록해쉬의 값을 받아올건지
     bool private mode =true ; //false == test mode ,true == real block hash use;
+
+    //  ??
     bytes32 public answerForTest;
 
     // 도박에 사용되는 고정된 값
@@ -29,9 +33,13 @@ contract Lottery {
 
     enum BlockStatus {checkable,NotRevealed,BlockLimitPassed}
     enum BettingResult {Fail,Win,Draw}
-    // 블록 선형 큐 
+
+
+    // 컨트랙트에서 자신에게 발생한 블록을 저장할 큐 
     uint256 private _tail;
     uint256 private _head;
+
+    // 문법 이해안됨
     mapping(uint256=>BetInfo) private _bets;
 
 
@@ -39,14 +47,16 @@ contract Lottery {
         owner=msg.sender;
     }
 
-// event log
+// 이벤트 로그 부분
   event BET(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBlockNumber);
   event WIN(uint256 index, address bettor, uint256 amount, byte challenges,byte answer, uint256 answerBlockNumber);
   event FAIL(uint256 index, address bettor, uint256 amount, byte challenges,byte answer, uint256 answerBlockNumber);
   event DRAW(uint256 index, address bettor, uint256 amount, byte challenges,byte answer, uint256 answerBlockNumber);
   event REFUND(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBlockNumber);
+
+
 // 스마트 컨트랙트에 들어있는 변수를가져오려면 view가 들어가야한다
-// 해당 값을 불러오는  getter
+// 현재 컨트랙트안에 저장되어있는  _pot값
     function getPot() public view returns (uint256 pot){
         return _pot;
     }
@@ -60,6 +70,36 @@ contract Lottery {
     *@param challenges 유저가 베팅하는 글자
     *@return 함수가 잘 수행되었는지 확인하는 bool 값
      */
+
+    // 실제로 dapp에서 실행시키려는 부분 
+
+
+// betAndDistribute는 정답을 입력하고 확인하는 코드이다 
+// 1. Dapp 에서 맞추려는 정답값을 betAndDistribute(challenges)에 포함시켜 send 를 한다
+// 2. betAndDistribute는 유저가 베팅하는 돈, 보낸사람, 트랜잭션이 담긴 블록 정보, 베팅한 정답중 challenges만 받으면된다. 나머지 돈 , 보낸사람 현재 블록 정보 는 msg객체에서 가져올 수 있다.
+// 3. challenges를 bet(challenges) 로 실행한다.
+// 3. require을 사용하여 베팅한 돈이 조건에 맞는지 확인한다
+// 4. require을 사용하여 pushBet(challenges)를 호출한다 ==> 컨트렉트에 저장할 부분
+// 5. pushBet에서 betInfo 라고 만든 constructor를 생성하여 값을 저장한다
+// 6. 저장할 값은 msg.sender , 현재 블록 + 3 ( 정답을 맞출 블록 값 ) , challenges ( 내가 입력한 값 )
+// 7. betInfo와 연결되어있는 _bet이라는 배열에 0번에 저장하고 +1 해준다  ( 변수 선언만하면 0으로 생성됨 )
+// 8. 정보를 저장하면 emit 을 사용하여 이벤트로 남긴다.
+// 9. 이벤트로 남긴 다음 전체 정답을 다시 확인한다.
+// 10 . betInfo와 연결된 블록 스택을 불러옴
+// 11.  첫 블록부터 스택끝까지 반복문으로 검사함
+// 11-1. 현재 블록번호가 스택에있는 betInfo[i]의 answerBlock보다 큰 값인지 getBlockStatus() 통해 확인함
+// 11-1-1 큰 경우 BlockStatus가 Checkable임으로 정답을 확인하는 절차를 진행함
+// 11-1-2 getAnswerBlockHash() 통해 해당 블록의 Hash를 가져옴
+// 11-1-3 불러온 해쉬를 isMatch에 대입하여 맞는지 맞지않는지 확인함 
+// 11-1-3-0 정답이 맞는 경우 
+// 11-1-3-1 누적상금과 , 베팅한 금액을 trasferAfterPayingFee 메소드의 addr.transfer을 통해 전달함
+// 11-1-3-2 상금을 0으로 초기화함
+// 11-1-3-3 이벤트를 남김
+// 11-1-3-4 정답이 틀린 경우
+// 11-1-3-5 베팅금액을 상금에 추가함
+// 11-1-3-6 이벤트를 남김
+// 11-1-4 popBet()을 호출하여 현재블록을 스택에서 delete 함 
+
     function betAndDistribute (byte challenges) public payable returns (bool result ){
         bet(challenges);
         distribute();
@@ -80,7 +120,7 @@ contract Lottery {
         // require 은 인수가 false로 평가되면 트랜잭션을 종료하고 ether balance를 되돌린다.
         // 보낸 금액과 요구조건 ( 0.005 eth) 가 맞는지 확인함
         require(msg.value==BET_AMOUNT, "Not enough ETH");
-
+        
         // push bet to the queue
         // 블록을에 담을 내용을 저장한다.
         require(pushBet(challenges), "Fail to add a new Bet Info");
@@ -109,36 +149,56 @@ contract Lottery {
         // 현재 블록 변수
         uint256 cur;
         uint256 transferAmount;
+
+        // pushBet에서 생성한 betInfo 객체 불러옴
         BetInfo memory b;
+
+        // enum상태로 만들어논 blockStatus는 checkable , notRevealed,PassLimit가 존재한다.
         BlockStatus currentBlockStatus;
+
         BettingResult currentBettingResult;
 
+        // 현재 _head (0) 부터 pushBet 에서 +1 한 값 ( _bet에 저장되어있는 블록의 갯수 만큼 반복함)
         for(cur=_head;cur<_tail;cur++){
+
+            // 첫번째 블록을 불러옴
             b=_bets[cur];
+
+
+            // 블록의 상태가 checkable인지 not Revealed인지 확인하는 절차로 블록의 정답을 확인할수 있는 상태인지 아닌지를 판단하는 부분이다
+            // ex )block.number가 5 이고  정답을 확인할 블록이 4일때   4 < 5 < 260 인 경우 checkable 이다
+            // ex )block.number가 4이고  정답을 확인할 블록이 4일때 NotRevealed 상태이다 
+
             currentBlockStatus = getBlockStatus(b.answerBlockNumber);
-            // checkable : block.number> answerBlockNumber && block.number -BLOCK_LMIT < answerBlockNumber  확인가능한 경우
+
+
+            // 블록 상태에따른 정답 맞추기 
+            // 1.. 정답을 맞춰볼수있는 상태라면 
             if(currentBlockStatus==BlockStatus.checkable){
              
-                bytes32 answerBlockHash = getAnswerBlockHash(b.answerBlockNumber);
-            //  currentBettingResult 
+             // 맞추려는 정답을 불러오는 getAnswerBlockHash메소드에 현재 블록 넘버를 넣고 블록 해쉬의 값을 가져온다
+             bytes32 answerBlockHash = getAnswerBlockHash(b.answerBlockNumber);
+
+
+                // 정답이 맞는지 블록에 들어있는 입력한 값과 블록의 해쉬를 비교한다 
              currentBettingResult=isMatch(b.challenges,answerBlockHash);
                 // 두개다 맞췄을경우 bettor gets pot
                  if(currentBettingResult==BettingResult.Win){
-                        //transper pot
+                        // 정답을 맞춘 유저에게 전달해야하는 돈 (누적상금 + 베팅한 금액 )
                         transferAmount= transferAfterPayingFee(b.bettor,_pot+BET_AMOUNT);
-                        //pot =0
+                        //누적상금을 초기화한다
                         _pot=0;
-                        //emit Win
+                        //이벤트를 남긴다.
+                        //  현재 인덱스 , 베팅자 , 상금 ,입력한 값 , 정답 , 맞추려는 블록 번호 
+                        // answerBlockHash[0] 의미 모르겟음 
                         emit WIN(cur,b.bettor,transferAmount,b.challenges,answerBlockHash[0],b.answerBlockNumber);
                  }
-
-
-               
-                // 틀릴경우bettor's moeny goes pot
+                // isMatch에서 정답이 틀린경우 bettingResult.Fail 임
                 if(currentBettingResult==BettingResult.Fail){
-                     //pot = pot+BET_AMOUNT
+                     //상금에 베팅금액을 추가함
                      _pot+=BET_AMOUNT;
-                     //emit Fail
+                    //  이벤트를 남긴다
+                    // 현재 인덱스 , 베팅자 , 상금 , 입력한 값 , 정답 , 맞추려는 블록 번호
                      emit FAIL(cur,b.bettor,0,b.challenges,answerBlockHash[0],b.answerBlockNumber);
                  }
 
@@ -148,7 +208,7 @@ contract Lottery {
                      //transfer only BET_AMOUNT
                      transferAmount= transferAfterPayingFee(b.bettor,BET_AMOUNT);
                      //emit Draw
-                        emit WIN(cur,b.bettor,transferAmount,b.challenges,answerBlockHash[0],b.answerBlockNumber);
+                        emit DRAW(cur,b.bettor,transferAmount,b.challenges,answerBlockHash[0],b.answerBlockNumber);
                  }
 
             }
@@ -163,15 +223,21 @@ contract Lottery {
                 //emit refund
                 emit REFUND(cur,b.bettor, transferAmount,b.challenges, b.answerBlockNumber);
             }
+
+            // 블록을 저장한 배열에서 블록을 꺼낸다
             popBet(cur);
         }
+        //검사할 블록 시작번호 변경 
         _head=cur;
     }
 
 // 도박에대한 수수료
 function transferAfterPayingFee(address payable addr, uint256 amount) internal returns(uint256){
+    // amount는 상금
     uint256 fee = 0;
     uint256 amountWithoutFee = amount-fee;
+
+    // 실제로 전송하는 부분
     addr.transfer(amountWithoutFee);
     owner.transfer(fee);
 
@@ -187,15 +253,14 @@ function transferAfterPayingFee(address payable addr, uint256 amount) internal r
 
 // 테스트 모드일대 임시로 정답을 지정해주는 메소드
     function setAnswerForTest(bytes32 answer) public returns (bool result){
-        
         require(msg.sender ==owner, "Only owner can set the answer for test mode");
-
         answerForTest= answer;
         return true;
     }
 
 // 현재 테스트 모드인지 리얼 블록해쉬값을 가져올건지 확인하는 메소드
     function getAnswerBlockHash(uint256 answerBlockNumber) internal view returns( bytes32 answer){
+        // mode 가 True일때 실제 블록의 해쉬를가져오고 , false일때 지정해논 정답값을 사용한다 
         return mode ? blockhash(answerBlockNumber) : answerForTest; 
     }
 
@@ -247,7 +312,7 @@ function transferAfterPayingFee(address payable addr, uint256 amount) internal r
 
     }
 
-// 블록 상태 
+// 블록의 상태를 판단하여 정답을 맞출수잇는 상태인지 아닌지를 확인한다 
     function getBlockStatus(uint256 answerBlockNumber) internal view returns (BlockStatus){
         if(block.number> answerBlockNumber && block.number < BLOCK_LIMIT + answerBlockNumber){
                 return BlockStatus.checkable;
@@ -262,7 +327,6 @@ function transferAfterPayingFee(address payable addr, uint256 amount) internal r
     }
 
         // check the answer
-
     function getBetInfo(uint256 index) public view returns (uint256 answerBlockNumber, address bettor, byte challenges){
         BetInfo memory b = _bets[index];
         answerBlockNumber=b.answerBlockNumber;
@@ -298,6 +362,7 @@ function transferAfterPayingFee(address payable addr, uint256 amount) internal r
         return true;
     }
 
+// _bet에 연결된 블록을 삭제함
     function popBet(uint256 index) internal returns (bool){
         delete _bets[index];
         return true;
